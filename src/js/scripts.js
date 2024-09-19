ef7dec783d4d5a4141d5f3e51ed3a8448e78ae7d
Kostyantin Kashcko <74814060+kostyantinkaschko@users.noreply.github.com>
2024-09-19 15:55:31 +0300

    da

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -43,7 +43,11 @@ let click = 0,
   slider = document.querySelector('.sliderBlock'),
   slides = document.querySelectorAll('#slider figcaption'),
   currentIndex = 0,
-  logo = document.querySelector("#logo")
+  logo = document.querySelector("#logo"),
+  bottomside = document.getElementById("bottomside"),
+  setActiveCategory = null,
+  idLogin = document.getElementById("login"),
+  passwordId = document.getElementById("password")
 document.addEventListener("DOMContentLoaded", event => {
   header.style.animation = "1.8s opacity"
   header.style.opacity = "1"
@@ -59,7 +63,20 @@ document.addEventListener("DOMContentLoaded", () => {
   footerContent.style.opacity = "0"
   footer.style.opacity = "0"
   getStartedButton.classList.toggle("disabledButton")
-
+  if (localStorage.buttonNumber) {
+    if (localStorage.buttonNumber === "firstButt") {
+      setActiveCategory = null
+    } else if (localStorage.buttonNumber === "secondButt") {
+      setActiveCategory = "Branding"
+    } else if (localStorage.buttonNumber === "thirdButt") {
+      setActiveCategory = "Design"
+    } else if (localStorage.buttonNumber === "fourButt") {
+      setActiveCategory = "Development"
+    } else if (localStorage.buttonNumber === "fiveButt") {
+      setActiveCategory = "Strategy"
+    }
+    activeButton(setActiveCategory, localStorage.buttonNumber)
+  }
   if (localStorage.username) {
     document.getElementById("name").value = localStorage.username
   }
@@ -76,48 +93,85 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("message").value = localStorage.message
   }
 
+  const popup = document.getElementById('popup')
+  let offsetX, offsetY, isDragging = false
+
+  popup.addEventListener('mousedown', (e) => {
+    offsetX = e.clientX - popup.getBoundingClientRect().left
+    offsetY = e.clientY - popup.getBoundingClientRect().top
+    isDragging = true
+    popup.style.cursor = "grab"
+    document.body.style.userSelect = "none"
+  })
+
+  document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+      let leftVar = `${e.clientX - offsetX + popup.clientHeight / 1.8}`,
+        topVar = `${e.clientY - offsetY + popup.clientWidth / 2.1}`
+      if (leftVar > 132.7857507324219 && leftVar < 1387.876437717014) {
+        popup.style.left = leftVar + "rem"
+      }
+      if (topVar < 608.9964999389648 && topVar > 126.92678557123456) {
+        popup.style.top = topVar + "rem"
+      }
+      localStorage.setItem('positionX', leftVar)
+      localStorage.setItem('positionY', topVar)
+      idLogin.setAttribute("readonly", true)
+      passwordId.setAttribute("readonly", true)
+      idLogin.style.text
+    }
+  })
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false
+    idLogin.removeAttribute("readonly")
+    passwordId.removeAttribute("readonly")
+    popup.style.cursor = "default"
+    document.body.style.userSelect = "text"
+  })
+
   function updateSlider() {
     let offset = -12.7 * currentIndex
-    if (offset <= -50 && window.innerWidth >= 1185) {
-      offset = -50.8
-      infinitySlider = true
-    } else if (offset <= -63.3 && window.innerWidth <= 1185) {
-      offset = -63.3
-      infinitySlider = true
-    }
     slider.style.transform = `translateX(${offset}%)`
-    if (infinitySlider) {
-      waitVarForAnimation = true
-      setTimeout(event => {
-        slider.style.transition = "none"
-        currentIndex = 0
-        slider.style.transform = `translateX(${offset}%)`
-        slider.style.transition = "all 1s ease"
-      }, 1800)
-    }
   }
+
   if (!waitVarForAnimation) {
     document.querySelector('#left').addEventListener('click', () => {
-      currentIndex = (currentIndex - 1)
+      currentIndex--
       if (currentIndex < 0) {
         currentIndex = 4
-        offset = -12.7 * currentIndex
         slider.style.transition = "none"
+        offset = -12.7 * currentIndex
         slider.style.transform = `translateX(${offset}%)`
-        setTimeout(event => {
+        setTimeout(() => {
           slider.style.transition = "all 1s ease"
           currentIndex = 3
-          offset = -12.7 * currentIndex
-          slider.style.transform = `translateX(${offset}%)`
+          updateSlider()
         }, 100)
+      } else {
+        updateSlider()
       }
-      updateSlider()
     })
+
     document.querySelector('#right').addEventListener('click', () => {
-      currentIndex = (currentIndex + 1)
-      updateSlider()
+      currentIndex++
+      if (currentIndex > 4) {
+        currentIndex = 0
+        slider.style.transition = "none"
+        offset = -12.7 * currentIndex
+        slider.style.transform = `translateX(${offset}%)`
+        setTimeout(() => {
+          slider.style.transition = "all 1s ease"
+          currentIndex = 1
+          offset = -12.7 * currentIndex
+          updateSlider()
+        }, 100)
+      } else {
+        updateSlider()
+      }
     })
   }
+
 })
 
 
@@ -158,13 +212,13 @@ function changeCite(number) {
   let currentCiteText = currentCite
   switch (currentCiteText) {
     case 1:
-      currentCiteText = "first"
+      currentCiteText = "firstCite"
       break
     case 2:
-      currentCiteText = "second"
+      currentCiteText = "secondCite"
       break
     case 3:
-      currentCiteText = "third"
+      currentCiteText = "thirdCite"
       break
 
   }
@@ -173,13 +227,14 @@ function changeCite(number) {
     numberCircle = number + "Circle",
     numberCiteVar = document.getElementById(numberCite),
     numberCircleVar = document.getElementById(numberCircle),
-    previousNumber = document.getElementById(currentCiteText + "Cite")
-  console.log(currentCiteText + "Cite")
+    previousNumber = document.getElementById(currentCiteText)
 
-  previousNumber.style.animation = 'opacity 0.5s reverse'
+  previousNumber.style.animation = 'opacity .5s reverse infinite'
+  console.log(previousNumber)
   setTimeout(() => {
     numberCiteVar.removeAttribute("style")
     previousNumber.removeAttribute("style")
+    console.log(previousNumber)
     document.getElementById("firstCite").classList.remove('activeCite')
     document.getElementById("secondCite").classList.remove('activeCite')
     document.getElementById("thirdCite").classList.remove('activeCite')
@@ -201,8 +256,7 @@ function changeCite(number) {
         break
 
     }
-    numberCiteVar.style.animation = 'opacity 0.5s'
-  }, 550)
+  }, 50)
 }
 
 function startCitesInterval() {
@@ -259,7 +313,7 @@ let tripleClickInterval = setInterval(() => {
 let parallaxInterval = setInterval(() => {
   header.style.backgroundPosition = `0 ${window.scrollY / 3}rem`
   propaboutwork.style.backgroundPosition = `0 ${(window.scrollY - 4900) / 3}rem`
-  if (topLine && scrollX < 49) {
+  if (topLine && scrollX < 49 && window.innerWidth >= 560) {
     topLine.class = 'top-line toplineFixed'
   }
 
@@ -278,33 +332,34 @@ let disabledButton = setInterval(() => {
 
 document.addEventListener("scroll", () => {
   let currentScrollY = window.scrollY
+  if (window.innerWidth >= 560) {
 
-  if (currentScrollY > lastScrollY) {
-    topLine.removeAttribute("style")
-    centerContent.removeAttribute("style")
-  } else {
-    if (currentScrollY > 700) {
-      topLine.style.transition = "all 1s ease"
-      topLine.style.position = "fixed"
-      centerContent.style.margin = "70rem 0 429rem 0"
-      if (window.innerWidth > 740) {
-        topLine.style.width = "93%"
-      } else {
-        topLine.style.width = "83%"
-      }
-      topLine.style.backgroundColor = "rgba(0,0,0,0.5)"
-      topLine.style.left = "0"
-      topLine.style.padding = "20rem 62rem"
-      topLine.style.top = "-40rem"
-      topLine.style.animation = "40s backgroundTopLine infinite"
-    } else {
+    if (currentScrollY > lastScrollY) {
       topLine.removeAttribute("style")
       centerContent.removeAttribute("style")
+    } else {
+      if (currentScrollY > 700) {
+        topLine.style.position = "fixed"
+        centerContent.style.margin = "70rem 0 429rem 0"
+        if (window.innerWidth > 740) {
+          topLine.style.width = "93%"
+        } else {
+          topLine.style.width = "83%"
+        }
+        topLine.style.padding = "20rem 62rem"
+        topLine.style.left = "0"
+        topLine.style.backgroundColor = "rgba(0,0,0,0.5)"
+        topLine.style.top = "-40rem"
+        topLine.style.transition = "all 1s ease"
+        topLine.style.animation = "40s backgroundTopLine infinite"
+      } else {
+        topLine.removeAttribute("style")
+        centerContent.removeAttribute("style")
+      }
     }
+
+    lastScrollY = currentScrollY
   }
-
-  lastScrollY = currentScrollY
-
   if (currentScrollY > 268) {
     services.style.opacity = "1"
     services.style.animation = "1s opacity"
@@ -317,23 +372,23 @@ document.addEventListener("scroll", () => {
     latestworksid.style.opacity = "1"
     latestworksid.style.animation = "1s opacity"
   }
-  if (currentScrollY > 2544) {
+  if (currentScrollY > 2544 - latestworksid.clientHeight) {
     proaboutwork.style.opacity = "1"
     proaboutwork.style.animation = "1s opacity"
   }
-  if (currentScrollY > 3200) {
+  if (currentScrollY > 3200 - latestworksid.clientHeight) {
     recentsPosts.style.opacity = "1"
     recentsPosts.style.animation = "1s opacity"
   }
-  if (currentScrollY > 4100) {
+  if (currentScrollY > 4100 - latestworksid.clientHeight) {
     companiesBlock.style.opacity = "1"
     companiesBlock.style.animation = "1s opacity"
   }
-  if (currentScrollY > 4700) {
+  if (currentScrollY > 4700 - latestworksid.clientHeight) {
     footerContent.style.opacity = "1"
     footerContent.style.animation = "1s opacity"
   }
-  if (currentScrollY > 5100) {
+  if (currentScrollY > 5100 - latestworksid.clientHeight) {
     footer.style.opacity = "1"
     footer.style.animation = "1s opacity"
   }
@@ -349,14 +404,14 @@ function send() {
     emailCheck = emailRexExp.test(email)
   if (emailCheck) {
     let userDataConfirmed = confirm('Чи правильно ви все вказали?')
+    localStorage.setItem("username", username)
+    localStorage.setItem("email", email)
+    localStorage.setItem("subject", subject)
+    localStorage.setItem("companyName", companyName)
+    localStorage.setItem("message", message)
   } else if (!emailCheck) {
     alert("You write uncorrect email")
   }
-  localStorage.setItem("username", username)
-  localStorage.setItem("email", email)
-  localStorage.setItem("subject", subject)
-  localStorage.setItem("companyName", companyName)
-  localStorage.setItem("message", message)
 }
 
 function readMore(id) {
@@ -392,17 +447,70 @@ function readLess() {
 
 }
 
-function activeButton() {
-  activeButtonVar = latestworksid.getElementById("activeButton")
+function activeButton(id, buttonNumber) {
+  let bottomsideFigures = document.querySelectorAll(".bottomside figure")
+
+  console.log(id)
+
+  if (id == null) {
+    document.querySelectorAll(".bottomside figcaption").forEach(element => {
+      element.classList.remove("hidden")
+      console.log(`Removed hidden from: ${element.outerHTML}`)
+    })
+  } else {
+    let found = false
+    bottomsideFigures.forEach(element => {
+      let figcaptions = element.querySelectorAll("figcaption")
+
+      figcaptions.forEach(figcaption => {
+        if (figcaption.classList.contains(id)) {
+          figcaption.classList.remove("hidden")
+          console.log(`Removed hidden from: ${figcaption.outerHTML}`)
+          found = true
+        } else {
+          figcaption.classList.add("hidden")
+          console.log(`Added hidden to: ${figcaption.outerHTML}`)
+        }
+      })
+    })
+  }
+
+  let buttonName = document.querySelector(`.${buttonNumber}`),
+    activeButtonVar = document.getElementById("activeButton")
+
   activeButtonVar.removeAttribute("id")
+
+
+  localStorage.setItem("buttonNumber", buttonNumber)
+
+  buttonName.setAttribute("id", "activeButton")
 }
+
+
+
+
 
 function popUp() {
+  if (!localStorage.positionX) {
+    localStorage.setItem("positionX", "50%")
+    popup.style.left = `${localStorage.positionX}`
+  } else {
+    popup.style.left = `${localStorage.positionX}`
+  }
+  if (!localStorage.positionY) {
+    localStorage.setItem("positionY", "50%")
+    popup.style.top = `${localStorage.positionY}`
+  } else {
+    popup.style.top = `${localStorage.positionY}`
+
+  }
   if (popUpOpenRights) {
     popup.classList.toggle("hide")
-    falshScrollBar.classList.toggle("hide")
+    if (window.innerWidth > 900) {
+      falshScrollBar.classList.toggle("hide")
+    }
     document.body.classList.toggle("no-scroll")
     headerWrapper.classList.toggle("margin")
+    centerContent.classList.toggle("margin2")
   }
 }
-
